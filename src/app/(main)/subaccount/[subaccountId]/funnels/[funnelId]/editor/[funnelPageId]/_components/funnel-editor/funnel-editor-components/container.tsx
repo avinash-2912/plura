e17c4c98db3d803af4b05f7f9080eac48db237f4
Avinash <ak7797405@gmail.com>
2024-05-08 22:56:44 +0530

    fixed embeded video

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor/funnel-editor-components/container.tsx
@@ -65,7 +65,7 @@ const Container = ({ element }: Props) => {
             containerId: id,
             elementDetails: {
               content: {
-                src: 'https://www.youtube.com/embed/A3l6YYkXzzg?si=zbcCeWcpq7Cwf8W1',
+                src: "https://www.youtube.com/embed/Rwe5Aw3KPHY?si=yGyRNDvKwTCZ2Nzc",
               },
               id: v4(),
               name: 'Video',
